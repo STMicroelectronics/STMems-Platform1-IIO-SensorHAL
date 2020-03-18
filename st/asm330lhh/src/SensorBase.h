59c5f67cac6492de25c69802896b5d68e790e061
mario tesi <mario.tesi@st.com>
2020-03-18 15:27:40 +0100

    Update Android makefile for Android Q build
    Signed-off-by: mario tesi <mario.tesi@st.com>

--- st/asm330lhh/src/SensorBase.h
+++ st/asm330lhh/src/SensorBase.h
@@ -34,7 +34,13 @@
 #include <hardware/sensors.h>
 
 #ifndef PLTF_LINUX_ENABLED
+
+#if CONFIG_ST_HAL_ANDROID_VERSION >= ST_HAL_OREO_VERSION
+#include <log/log.h>
+#else
 #include <cutils/log.h>
+#endif /* use log/log.h start from android 8 major version */
+
 #include <utils/SystemClock.h>
 #else
 #include <stdio.h>
