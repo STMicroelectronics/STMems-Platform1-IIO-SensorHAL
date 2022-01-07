c0d7f89e5de0bc6742d845bf787b963ea91351c4
Mario Tesi <mario.tesi@st.com>
2022-01-07 11:29:34 +0100

    Adedd support to ignition off setting from config file
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

--- st/src/iNotifyConfigMngmt.h
+++ st/src/iNotifyConfigMngmt.h
@@ -10,6 +10,8 @@
 #ifndef __HAL_INOTIFY_CONFIGURATION
 #define __HAL_INOTIFY_CONFIGURATION
 
+#include "SensorHAL.h"
+
 #define HAL_CONFIGURATION_FILE	"hal_config"
 #define HAL_CONFIGURATION_PATH	"/etc/sensorhal/"
 
@@ -20,10 +22,12 @@ enum PARSING_STRING_INDEX {
 	ALGO_TOWING_JACK_MIN_DURATION_INDEX,
 	ALGO_CRASH_IMPACT_TH_INDEX,
 	ALGO_CRASH_MIN_DURATION_INDEX,
+	IGNITION_OFF_INDEX,
 };
 
 struct thread_params_t {
 	char *pathname;
+	STSensorHAL_data *hal_data;
 };
 
 struct sensor_placement_t {
@@ -37,10 +41,11 @@ struct hal_config_t {
 	uint32_t algo_towing_jack_min_duration;
 	uint16_t algo_crash_impact_th;
 	uint32_t algo_crash_min_duration;
+	uint32_t ignition_off;
 	int loglevel;
 };
 
-int init_notify_loop(char *pathname);
+int init_notify_loop(char *pathname, STSensorHAL_data *hal_data);
 
 const struct hal_config_t get_config(void);
 
